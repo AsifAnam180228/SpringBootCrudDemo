594d761d6016283f874d10dbde4018f4b749ce19
Asif Anam <anam1828@cseku.ac.bd>
2023-11-06 16:31:01 +0600

    video 126 2:30min

--- src/main/java/com/asifanam/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/asifanam/cruddemo/rest/EmployeeRestController.java
@@ -1,9 +1,7 @@
 package com.asifanam.cruddemo.rest;
 import com.asifanam.cruddemo.entity.Employee;
 import com.asifanam.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +20,31 @@ public class EmployeeRestController {
     //expose "/employees" and return a list of employee
     @GetMapping("/employees")
     public List<Employee> findAll(){
+
         return employeeService.findAll();
     }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null){
+            throw new RuntimeException("Employee not found - " + employeeId);
+        }
+        return theEmployee;
+    }
+    //add mapping for POST/employees - add new employee
+
+    @PostMapping("/employees/{employeeId}")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        //also just in case they pass an id in JSON  .. set id to 0
+        //this is to focus a save of new item .. instead of update
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
 
 }
