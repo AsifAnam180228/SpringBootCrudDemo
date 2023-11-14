85aa616f79ee816a9beaa997bf36b19f43cee0f0
Asif Anam <anam1828@cseku.ac.bd>
2023-11-14 16:34:47 +0600

    done

---
+++ src/main/java/com/asifanam/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.asifanam.cruddemo.dao;
+
+import com.asifanam.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
